dbea8a51419ba0410787137e3be3acb2e90b42a3
Phan Hoang <17520519@gm.uit.edu.vn>
2024-08-14 17:05:26 +0700

    new

---
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/GamingAppLauncherApplication.java
@@ -0,0 +1,21 @@
+package com.in28minutes.learn_spring_framework;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.in28minutes.learn_spring_framework.game.GameRunner;
+
+@Configuration
+@ComponentScan("com.in28minutes.learn_spring_framework.game")
+public class GamingAppLauncherApplication {
+    public static void main(String[] args) {
+        try (var context = new AnnotationConfigApplicationContext(GamingAppLauncherApplication.class)) {
+            context.getBean(GameRunner.class).run();
+        } catch (BeansException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
---
+++ learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/helloworld/App02HelloWorldSpring.java
@@ -0,0 +1,16 @@
+package com.in28minutes.learn_spring_framework;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Arrays;
+
+public class App02HelloWorldSpring {
+    public static void main (String[] args) {
+        var context = new AnnotationConfigApplicationContext( HelloWorldConfiguration.class );
+//        System.out.println( context.getBean( "name" ) );
+//        System.out.println( context.getBean( "person" ) );
+//        System.out.println( context.getBean( "address" ) );
+//        System.out.println( context.getBean( "person2MethodCall" ) );
+        Arrays.stream( context.getBeanDefinitionNames() ).forEach( System.out::println );
+    }
+}
